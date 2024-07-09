94a005301764a203099431b3a1ffd5196e0cbb13
Fearocanity (けろさん) <91414643+fearocanity@users.noreply.github.com>
2024-07-09 10:24:18 +0900

    wtf

--- autorun.sh
+++ autorun.sh
@@ -6,7 +6,7 @@ while [[ "${date}" -gt "$(date +%s)" ]]; do
     curl -sLf -H "Authorization: Bearer ${1}" \
       -H "Accept: application/vnd.github.v3+json" \
       -X POST \
-      -d '{"ref":"'"${3}"'","inputs":{}}' "https://api.github.com/repos/fearocanity/fearocanity/actions/workflows/${2}/dispatches" \
+      -d '{"ref":"master","inputs":{}}' "https://api.github.com/repos/fearocanity/fearocanity/actions/workflows/${2}/dispatches" \
       -o /dev/null && { : "$((i+=1))" ; printf '%s\n' "Completed Runs: ${i:=0}" ;}
     sleep 210
 done
